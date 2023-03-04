be8c08ae2828d5569e09a3c34dfa15962c9af014
RoelGD <roelderion15@gmail.com>
2023-03-04 17:52:12 +0800

    my first push

---
+++ src/Footer.js
@@ -0,0 +1,23 @@
+import './Footer.css';
+
+const Footer = () =>{
+  
+  const today = new Date ();
+  
+  return(
+
+    <footer>
+      <p>
+
+      Copyright &copy; {today.getFullYear()}
+
+      </p>
+   
+    </footer>
+
+
+
+  )
+}
+
+export default Footer;
